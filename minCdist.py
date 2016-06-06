6fffe11a8e7e716445c3661dedb9fb2bde281d8a
JohnYeabinMoon <jym.econ@gmail.com>
2016-06-06 13:36:51 -0400

    Create minCdist.py

---
+++ minCdist.py
@@ -0,0 +1,27 @@
+def minCdist(thetaVec, data):
+	nPers=2;
+	
+	# Note that thetaVec starts at the initialValues
+	
+	scaleFac0_mho=thetaVec[0];
+	scaleFac1_mho=thetaVec[1];
+	scaleFac0_CEA=thetaVec[2];
+	scaleFac1_CEA=thetaVec[3];
+	thetaBase=thetaVec[4];
+	
+	#########################
+	# data frame work should be changed.#####
+	wyRatSeries=data(:,1);
+	mhoSeries=data(:,2);
+	CEAseries=data(:,3);
+	saving_rate=data(:,4);
+	#########################
+	
+	steadyStateMC=[]; 
+	steadyStateMCexPDVdebt=[];
+
+	debtLimPDVrescaled=scaleFac0_CEA+scaleFac1_CEA*CEAseries; # equation (7)	
+	mhoRescaled=scaleFac0_mho+scaleFac1_mho*mhoSeries;  # equation (8)
+	
+
+
