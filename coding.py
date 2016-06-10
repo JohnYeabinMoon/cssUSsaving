35f53724b0131ecd85665232099fa61094d2c1fc
Yeabin Moon <jym.econ@gmail.com>
2016-06-09 21:41:11 -0400

    success_coding
    a bit tedious work..
    but sucess!!

--- coding.py
+++ coding.py
@@ -12,16 +12,36 @@ wyRat = dataOrig.wyRat
 CEA = dataOrig.CEA
 unemp_pred = dataOrig.unemp_pred
 
-wyRatSeries = wyRat[24:202];
-mhoSeries=unemp_pred[25:203]/100;
-CEAseries=CEA[25:203];
-time=time[25:203];
+wyRatSeries = wyRat[24:203];
+mhoSeries=unemp_pred[25:204]/100;
+CEAseries=CEA[25:204];
+time=time[25:204];
 thetaBase=0.05;
 
+## Need to reset index to merge 
+wyRatSeries = wyRatSeries.reset_index()
+mhoSeries = mhoSeries.reset_index()
+CEAseries = CEAseries.reset_index()
+saving_rate = saving_rate.reset_index()
+
+# merge mhoSeries CEAseries and saving_rate
+mhoSeries = mhoSeries.merge(CEAseries)
+mhoSeries = mhoSeries.merge(saving_rate)
+
+wyRatSeries = wyRatSeries.wyRat # delete current index
+wyRatSeries = wyRatSeries.reset_index() # New index starting at 0
+mhoSeries = mhoSeries[['unemp_pred','CEA','saving_rate']] # delete current index
+mhoSeries = mhoSeries.reset_index() # New index starting at 0
+
+# Merge wyRatSeries and the rest 
+data = wyRatSeries.merge(mhoSeries) 
+data = data[['wyRat','unemp_pred','CEA','saving_rate']] # delete index column
+
+# rename labels
+data.columns = 'wyRatSeries','mhoSeries','CEAseries','saving_rate'
+
+
 # cExAll=[]; cEyAll=[]; mcPathAll=[]; scriptmEBaseAll=[]; steadyStateMC=[]; PiAll=[];
-
-data = np.array([wyRatSeries, mhoSeries, CEAseries, saving_rate[25:203]])
-
 
 
 initialValues = np.array([0, .0003, 1, 4, .01]) 
