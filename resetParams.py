79fde27b11db3ac5ca0b4a591574ad11ed427150
Yeabin Moon <jym.econ@gmail.com>
2016-06-20 04:17:41 -0400

    Delete resetParams.py

--- resetParams.py
+++
@@ -1,9 +0,0 @@
-mho = mhoBase;
-theta = thetaBase;
-littleR = littleRBase;
-littleG = littleGBase;
-rho = rhoBase;
-
-if VerboseOutput==1:
-	print 'Parameters have been reset to their base values.'
-	
